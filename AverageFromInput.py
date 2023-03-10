0ceb3dbca41dc994b0bf93a86c980b40daa34b78
michael-sherrer <114097258+michael-sherrer@users.noreply.github.com>
2023-03-09 22:58:37 -0500

    Average from Input with Exception

--- AverageFromInput.py
+++ AverageFromInput.py
@@ -1,18 +1,42 @@
 #Michael Sherrer
 #CS 175L
-#AverageFromInput Assignment
+#AverageFromInput Assignment with Exception
 
-def main():
-    total=0
-    read_in=0
+def values():
     numbers_file=open('numbers.txt','r')
-    for line in numbers_file:
+    line=numbers_file.readline()
+    while line!='':
         current_number=float(line)
-        read_in+=1
-        total+=current_number
-        print('I read in',read_in,'numbers(s), Current number is:',
-              current_number,'Total is:',total)
+        line=numbers_file.readline()
+        return current_number
     numbers_file.close()
 
+def average():
+    num_lines=0
+    total=0.0
+    numbers_file=open('numbers.txt','r')
+    line=numbers_file.readline()
+    for line in numbers_file:
+        current_number=float(line)
+        total+=current_number
+        num_lines+=1
+    average=total/num_lines
+    return average
+
+def error_check(average):
+    print('Average of the numbers:',average)
+    try:
+        numbers_file=open('numbers.txt','r')
+    except IOError:
+        print('An error occured trying to read the file.')
+    except ValueError:
+        print('Non-numeric data found in the file.')
+    
+
+def main():
+    values()
+    average()
+    error_check()
+    
 if __name__=='__main__':
     main()
